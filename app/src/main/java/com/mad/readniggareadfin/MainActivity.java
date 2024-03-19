bf905e006bdaf6d51dfeca67b90a3e7940972e99
0-pyro <apatade76@gmail.com>
2024-03-19 22:01:12 +0530

    added login and registration

--- app/src/main/java/com/mad/readniggareadfin/MainActivity.java
+++ app/src/main/java/com/mad/readniggareadfin/MainActivity.java
@@ -2,6 +2,7 @@ package com.mad.readniggareadfin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello");
+        ;startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
     }
 }
