4cad0c17f78ab916bfb79797b845b4a30eedf465
0-pyro <apatade76@gmail.com>
2024-03-19 21:53:52 +0530

    added login and registration

---
+++ app/src/main/java/com/mad/readniggareadfin/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.mad.readniggareadfin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        FirebaseAuth auth;
+        EditText mail, pass;
+        Button redir;
+        Button login;
+        auth = FirebaseAuth.getInstance();
+        mail = findViewById(R.id.email);
+        pass = findViewById(R.id.pass);
+        redir = findViewById(R.id.redirbutton);
+        login = findViewById(R.id.button);
+        redir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mail.getText().toString();
+                String pw = pass.getText().toString();
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if(!pw.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, pw).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/mad/readniggareadfin/RegistrationActivity.java
+++ app/src/main/java/com/mad/readniggareadfin/RegistrationActivity.java
@@ -1,26 +1,69 @@
 package com.mad.readniggareadfin;
 
-import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.mad.readniggareadfin.databinding.ActivityRegistrationBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
+        EditText email, pass;
+        Button button;
+        Button redir;
+        email = findViewById(R.id.mail);
+        pass = findViewById(R.id.pass);
+        button = findViewById(R.id.button);
+        redir = findViewById(R.id.redirbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = email.getText().toString().trim();
+                String passw = pass.getText().toString().trim();
 
-
+                if(user.isEmpty()) {
+                    email.setError("Enter mail");
+                }
+                if(passw.isEmpty()) {
+                    pass.setError("Enter pass");
+                }
+                else {
+                    mAuth.createUserWithEmailAndPassword(user, passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(RegistrationActivity.this, "Registration Success", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                            } else {
+                                Toast.makeText(RegistrationActivity.this, "Registraion Fail" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        redir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
