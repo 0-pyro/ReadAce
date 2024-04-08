1ec443841b3320430d6bc86aa5f33905e3ef5240
Anshika <arekaranshika3017@gmail.com>
2024-04-08 10:17:32 +0530

    aaaaa

--- app/src/main/java/com/mad/readniggareadfin/LoginFragment.java
+++ app/src/main/java/com/mad/readniggareadfin/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.mad.readniggareadfin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        startActivity(new Intent(this.getActivity(), LoginActivity.class));
         return inflater.inflate(R.layout.activity_login, container, false);
     }
 }
